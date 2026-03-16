481edb11b4d9026a620fb2d804e35ade8df93a1a
Hugo Silva <jose.silva@extreme.digital>
2026-03-16 12:19:16 -0300

    feat: Adding new billing microservice

---
+++ invoicing/src/main/java/io/github/yhugorocha/invoicing/InvoicingApplication.java
@@ -0,0 +1,13 @@
+package io.github.yhugorocha.invoicing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicingApplication.class, args);
+	}
+
+}
